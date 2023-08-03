9704c76f104e727c3615f6b9f17cad71c30f36a8
order&chaos <sean@orderandchaoscreative.com>
2023-08-03 22:47:29 +0100

    Add region

--- bin/deployment-api.ts
+++ bin/deployment-api.ts
@@ -5,6 +5,7 @@ import { DeploymentApiStack } from '../lib/deployment-api-stack';
 
 const app = new cdk.App();
 new DeploymentApiStack(app, 'DeploymentApiStack', {
+    env: {region: 'eu-west-1'},
     certificateDomainNameParameterName: '/api/certificateDomainName',
     hostedZoneIdParameterName: '/api/hostedZoneId',
     hostedZoneNameParameterName: '/api/hostedZoneName',
